b4a257d8c6a25dfb0b19583a07251950835bf192
Wendy Sanarwanto <saintc0d3r@gmail.com>
2016-01-06 10:39:53 +0800

    Added Project 1: Storm Viewer

---
+++ project1_storm_viewer/project1_storm_viewer/DetailViewController.swift
@@ -0,0 +1,52 @@
+//
+//  DetailViewController.swift
+//  project1_storm_viewer
+//
+//  Created by Wendy Sanarwanto on 1/6/16.
+//  Copyright © 2016 Wendy Sanarwanto. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailImageView: UIImageView!
+    
+    var detailItem: String? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = self.detailItem {
+            if let image_view = self.detailImageView{
+                image_view.image = UIImage(named: detail)
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        self.navigationController?.hidesBarsOnTap = false
+    }
+}
+
---
+++ project1_storm_viewer/project1_storm_viewer/MasterViewController.swift
@@ -0,0 +1,89 @@
+//
+//  MasterViewController.swift
+//  project1_storm_viewer
+//
+//  Created by Wendy Sanarwanto on 1/6/16.
+//  Copyright © 2016 Wendy Sanarwanto. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UITableViewController {
+
+    var detailViewController: DetailViewController? = nil
+    var objects = [String]()
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        load_images_list()
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        self.clearsSelectionOnViewWillAppear = self.splitViewController!.collapsed
+        super.viewWillAppear(animated)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Segues
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showDetail" {
+            if let indexPath = self.tableView.indexPathForSelectedRow {
+                let navigation_controller = segue.destinationViewController as! UINavigationController
+                let controller = navigation_controller.topViewController as! DetailViewController
+                controller.detailItem = objects[indexPath.row]
+                
+                controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
+                controller.navigationItem.leftItemsSupplementBackButton = true
+            }
+        }
+    }
+
+    // MARK: - Table View
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return objects.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+
+        let object = objects[indexPath.row]
+        cell.textLabel!.text = object
+        return cell
+    }
+
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    
+    func load_images_list(){
+        // instantiate file manager object
+        let file_manager = NSFileManager.defaultManager()
+        
+        // refer to main bundle and get the main bundle's resource path
+        let resource_path = NSBundle.mainBundle().resourcePath
+        
+        // get contents inside the resource path
+        let contents = try! file_manager.contentsOfDirectoryAtPath(resource_path!)
+        
+        // Iterate on each files inside the resource path
+        for content in contents{
+            if content.hasPrefix("nssl"){
+                objects.append(content)
+            }
+        }
+    }
+}
+
